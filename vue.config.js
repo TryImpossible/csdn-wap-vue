f61f346c47a150bc68077192c26230daed131734
Barry <865068275@qq.com>
2020-03-17 12:39:06 +0800

    新建工程

---
+++ vue.config.js
@@ -0,0 +1,68 @@
+module.exports = {
+  // 部署应用包时的基本 URL
+  // 例如，如果你的应用被部署在 https://www.my-app.com/my-app/，则设置 publicPath 为 /my-app/
+  // 这个值也可以被设置为空字符串 ('') 或是相对路径 ('./')，这样所有的资源都会被链接为相对路径，这样打出来的包可以被部署在任意路径
+  publicPath: '/',
+  // 当运行 vue-cli-service build 时生成的生产环境构建文件的目录
+  outputDir: 'dist',
+  // 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
+  assetsDir: '',
+  // 指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径
+  indexPath: 'index.html',
+  // 默认情况下，生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存。然而，这也要求 index 的 HTML 是被 Vue CLI 自动生成的
+  filenameHashing: true,
+  // 在 multi-page 模式下构建应用。每个“page”应该有一个对应的 JavaScript 入口文件。
+  pages: {
+    index: {
+      // page 的入口
+      entry: 'src/main.js',
+      // 模板来源
+      template: 'public/index.html',
+      // 在 dist/index.html 的输出
+      filename: 'index.html',
+      // 当使用 title 选项时，
+      // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Index Page',
+      // 在这个页面中包含的块，默认情况下会包含
+      // 提取出来的通用 chunk 和 vendor chunk。
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    }
+    // 当使用只有入口的字符串格式时，
+    // 模板会被推导为 `public/subpage.html`
+    // 并且如果找不到的话，就回退到 `public/index.html`。
+    // 输出文件名会被推导为 `subpage.html`。
+    // subpage: 'src/subpage/main.js'
+  },
+  // 是否在开发环境下通过 eslint-loader 在每次保存时 lint 代码。这个值会在 @vue/cli-plugin-eslint 被安装之后生效
+  lintOnSave: true,
+  // 是否使用包含运行时编译器的 Vue 构建版本。设置为 true 后你就可以在 Vue 组件中使用 template 选项了，但是这会让你的应用额外增加 10kb 左右
+  runtimeCompiler: false,
+  // 默认情况下 babel-loader 会忽略所有 node_modules 中的文件。如果你想要通过 Babel 显式转译一个依赖，可以在这个选项中列出来
+  transpileDependencies: [],
+  // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建
+  productionSourceMap: true,
+  // 调整内部的 webpack 配置。
+  // 查阅 https://cli.vuejs.org/zh/guide/webpack.html#%E7%AE%80%E5%8D%95%E7%9A%84%E9%85%8D%E7%BD%AE%E6%96%B9%E5%BC%8F
+  // chainWebpack: () => {},
+  // configureWebpack: () => {},
+  devServer: {
+    port: 8080, // 端口号
+    // host: 'localhost',
+    https: false, // https:{type:Boolean}
+    open: false //配置自动启动浏览器
+    // 查阅：https://github.com/chimurai/http-proxy-middleware#proxycontext-config
+    // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
+    // proxy: {
+    //   '/api': {
+    //     target: '<url>',
+    //     ws: true,
+    //     changeOrigin: true
+    //   },
+    //   '/foo': {
+    //     target: '<other_url>'
+    //   }
+    // } // 配置多个代理
+  },
+  // 是否为 Babel 或 TypeScript 使用 thread-loader
+  parallel: require('os').cpus().length > 1
+};
